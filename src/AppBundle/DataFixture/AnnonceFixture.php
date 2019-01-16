36f60a53e2d9d60c7cf686395f490c50927e8552
Sébastien Criquet <sebastien48criquet@gmail.com>
2019-01-16 09:34:16 +0100

    update

--- src/AppBundle/DataFixture/AnnonceFixture.php
+++ src/AppBundle/DataFixture/AnnonceFixture.php
@@ -14,7 +14,7 @@ class AnnonceFixture extends Fixture {
      * @param ObjectManager $manager
      */
     public function load(ObjectManager $manager) {
-        for($i = 0; $i <= 10000000; $i++) {
+        for($i = 0; $i <= 10000; $i++) {
             $annonce = new Annonce();
             $annonce -> setName($i . "Portable 359 pouces royal de luxe, compatbile Fortnite");
             $annonce -> setDescription($i . "Se portabl manifiq, vs prokura dé sensasion innedit qd vs vs rigolé a se je merveiille sur se fantastik je ké Fortnite ");
