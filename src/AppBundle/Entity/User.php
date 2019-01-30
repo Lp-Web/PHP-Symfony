bdf066d4fc57c6304f38e85e93374e26c2e8901a
Naopil <naopil88@gmail.com>
2019-01-30 09:24:30 +0100

    Update FOSUserBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leona
+ * Date: 30/01/2019
+ * Time: 09:07
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
